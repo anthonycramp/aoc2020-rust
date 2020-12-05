5b20d6cc7431b545e895b0cffc42d0eb2499f247
Anthony Cramp <anthony.cramp@gmail.com>
2020-12-05 23:05:25 +1030

    Added alternate solution
    Mapped boarding pass to binary and parsed that.

---
+++ day05/README.md
@@ -0,0 +1,85 @@
+# AOC 2020 Day 05
+
+https://adventofcode.com/2020/day/5
+
+https://www.reddit.com/r/rust/comments/k71r9n/advent_of_code_2020_day_5/
+
+## My Solution
+
+Fairly standard "guessing game" solution. Solved both parts in about 30 min.
+Rank approx 4200.
+
+```rust
+fn compute_seat_id(boarding_pass: &str) -> i32 {
+    let (row, col) = compute_seat_row_col(&boarding_pass);
+    row * 8 + col
+}
+
+fn compute_seat_row_col(boarding_pass: &str) -> (i32, i32) {
+    let row_code = &boarding_pass[0..7];
+    let col_code = &boarding_pass[7..];
+
+    let mut low = 0;
+    let mut high = 127;
+
+    for code in row_code.chars() {
+        match code {
+            'F' => high = low + (high - low) / 2,
+            'B' => low = low + (high - low) / 2,
+            _ => panic!("Unknown row code: {}", code),
+        }
+    }
+
+    let row = high;
+
+    let mut low = 0;
+    let mut high = 7;
+
+    for code in col_code.chars() {
+        match code {
+            'L' => high = low + (high - low) / 2,
+            'R' => low = low + (high - low) / 2,
+            _ => panic!("Unknown row code: {}", code),
+        }
+    }
+
+    let col = high;
+
+    (row, col)
+}
+```
+
+## Alternate solution
+
+Musing after the fact, I was thinking that the boarding pass code could be
+converted to binary and then decimal to yield the seat row and column.
+Eyeballing it now, it looks like it will work.
+
+It does work:
+
+```rust
+fn compute_seat_row_col_binary(boarding_pass: &str) -> (i32, i32) {
+    let row_code = &boarding_pass[..7];
+    let row_code_binary = row_code
+        .chars()
+        .map(|c| match c {
+            'F' => '0',
+            _ => '1',
+        })
+        .collect::<String>();
+
+    let row = i32::from_str_radix(row_code_binary.as_str(), 2).expect("Error decoding binary row");
+
+    let col_code = &boarding_pass[7..];
+    let col_code_binary = col_code
+        .chars()
+        .map(|c| match c {
+            'L' => '0',
+            _ => '1',
+        })
+        .collect::<String>();
+    let col = i32::from_str_radix(col_code_binary.as_str(), 2).expect("Error decoding binary col");
+
+    (row, col)
+}
+```
--- day05/src/main.rs
+++ day05/src/main.rs
@@ -11,7 +11,7 @@ fn main() {
 fn run_part1(input: &str) -> i32 {
     input
         .lines()
-        .map(|line| compute_seat_id(line))
+        .map(|line| compute_seat_id(line, compute_seat_row_col_binary))
         .max()
         .expect("Unexpected")
 }
@@ -19,7 +19,7 @@ fn run_part1(input: &str) -> i32 {
 fn run_part2(input: &str) -> i32 {
     let mut seat_ids = input
         .lines()
-        .map(|line| compute_seat_id(line))
+        .map(|line| compute_seat_id(line, compute_seat_row_col_binary))
         .collect::<Vec<_>>();
     seat_ids.sort();
     let mut iter = seat_ids.iter_mut();
@@ -34,8 +34,8 @@ fn run_part2(input: &str) -> i32 {
     *first + 1
 }
 
-fn compute_seat_id(boarding_pass: &str) -> i32 {
-    let (row, col) = compute_seat_row_col(&boarding_pass);
+fn compute_seat_id(boarding_pass: &str, compute_row_col_fn: fn(&str) -> (i32, i32)) -> i32 {
+    let (row, col) = compute_row_col_fn(&boarding_pass);
     row * 8 + col
 }
 
@@ -72,6 +72,31 @@ fn compute_seat_row_col(boarding_pass: &str) -> (i32, i32) {
     (row, col)
 }
 
+fn compute_seat_row_col_binary(boarding_pass: &str) -> (i32, i32) {
+    let row_code = &boarding_pass[..7];
+    let row_code_binary = row_code
+        .chars()
+        .map(|c| match c {
+            'F' => '0',
+            _ => '1',
+        })
+        .collect::<String>();
+
+    let row = i32::from_str_radix(row_code_binary.as_str(), 2).expect("Error decoding binary row");
+
+    let col_code = &boarding_pass[7..];
+    let col_code_binary = col_code
+        .chars()
+        .map(|c| match c {
+            'L' => '0',
+            _ => '1',
+        })
+        .collect::<String>();
+    let col = i32::from_str_radix(col_code_binary.as_str(), 2).expect("Error decoding binary col");
+
+    (row, col)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -80,6 +105,14 @@ mod tests {
     fn test_compute_seat_row_col1() {
         let boarding_pass = "BFFFBBFRRR";
         let seat_row_col = compute_seat_row_col(&boarding_pass);
+        assert_eq!(seat_row_col.0, 70);
+        assert_eq!(seat_row_col.1, 7);
+    }
+
+    #[test]
+    fn test_compute_seat_row_col_binary1() {
+        let boarding_pass = "BFFFBBFRRR";
+        let seat_row_col = compute_seat_row_col_binary(&boarding_pass);
         assert_eq!(seat_row_col.0, 70);
         assert_eq!(seat_row_col.1, 7);
     }
