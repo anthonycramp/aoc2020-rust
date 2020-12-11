4ea09a90087ef161b2417e80e1121a607a946da0
Anthony Cramp <anthony.cramp@gmail.com>
2020-12-12 02:57:00 +1030

    Day 08 Part 2 Solution

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -2,11 +2,17 @@ const INPUT: &str = include_str!("../../resources/day08_input.txt");
 
 fn main() {
     let mut computer = Computer::from(INPUT);
-    computer.run_until_loop();
+    computer.run();
 
     println!("Day 08 Part 1: {}", computer.accumulator());
+
+    let mut computer = Computer::from(INPUT);
+    computer.fix();
+    computer.run();
+    println!("Day 08 Part 2: {}", computer.accumulator());
 }
 
+#[derive(Clone)]
 enum Instruction {
     NOP(i32),
     ACC(i32),
@@ -26,6 +32,12 @@ impl From<&str> for Instruction {
     }
 }
 
+enum RunResult {
+    OK,
+    LOOP,
+}
+
+#[derive(Clone)]
 struct Computer {
     accumulator: i32,
     instructions: Vec<Instruction>,
@@ -37,15 +49,18 @@ impl Computer {
         self.accumulator
     }
 
-    fn run_until_loop(&mut self) {
+    fn run(&mut self) -> RunResult {
         let mut pointer_history = vec![];
         loop {
+            if self.pointer as usize >= self.instructions.len() {
+                return RunResult::OK;
+            }
+
             if pointer_history.contains(&self.pointer) {
-                break;
+                return RunResult::LOOP;
             } else {
                 pointer_history.push(self.pointer)
             }
-
             let instruction = &self.instructions[self.pointer as usize];
             match instruction {
                 Instruction::NOP(_) => self.pointer += 1,
@@ -54,6 +69,32 @@ impl Computer {
                     self.pointer += 1;
                 }
                 Instruction::JMP(jmp) => self.pointer += jmp,
+            }
+        }
+    }
+
+    fn fix(&mut self) {
+        let mut last_pointer_fixed = 0;
+
+        loop {
+            let mut computer_clone = self.clone();
+
+            match self.instructions[last_pointer_fixed] {
+                Instruction::NOP(op) => {
+                    computer_clone.instructions[last_pointer_fixed] = Instruction::JMP(op)
+                }
+                Instruction::JMP(op) => {
+                    computer_clone.instructions[last_pointer_fixed] = Instruction::NOP(op)
+                }
+                _ => (),
+            }
+
+            match computer_clone.run() {
+                RunResult::LOOP => last_pointer_fixed += 1,
+                RunResult::OK => {
+                    self.instructions = computer_clone.instructions;
+                    break;
+                }
             }
         }
     }
@@ -86,7 +127,25 @@ mod tests {
         acc +6";
 
         let mut computer = Computer::from(input);
-        computer.run_until_loop();
+        computer.run();
         assert_eq!(computer.accumulator(), 5);
+    }
+
+    #[test]
+    fn test2() {
+        let input = "nop +0
+        acc +1
+        jmp +4
+        acc +3
+        jmp -3
+        acc -99
+        acc +1
+        jmp -4
+        acc +6";
+
+        let mut computer = Computer::from(input);
+        computer.fix();
+        computer.run();
+        assert_eq!(computer.accumulator(), 8);
     }
 }
